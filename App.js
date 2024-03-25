349e6294e13fb9a17ea17dfefe3c64189f29914e
Murilo Gois <murilo.almeida@calindra.com.br>
2024-03-25 17:01:02 -0300

    feat: Add track-player

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StatusBar, StyleSheet } from "react-native";
+import MusicPlayer from "./MusicPlayer";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.container}>
+      <StatusBar barStyle="light-content" />
+      <MusicPlayer />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
+export default App;
+
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
